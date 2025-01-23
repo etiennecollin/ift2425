6d2070b2ff26b23c87b59ea3330a6e7c8cf6fbb0
etiennecollin <collin.etienne.contact@gmail.com>
2025-01-23 17:00:33 -0500

    Improved compile script

--- tp1/compilegpp.sh
+++ tp1/compilegpp.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-echo ""
-echo "compilation du programme ${a=${1}.c}"
+file="$1"
 
-g++ -o $1 $a -lm -lX11
+# Make sure the file exists
+if [ ! -f "$file" ]; then
+    echo "File does not exist"
+    exit 1
+fi
+
+# Make sure the file is a cpp file
+if [[ "$file" != *.cpp ]]; then
+    echo "File is not a cpp file"
+    exit 1
+fi
+
+file_no_ext="$(basename $1 .cpp)"
+echo "Compiling ${file}"
+
+# Get output code of the following command
+g++ -o "$file_no_ext" "$file" -lm -lX11
+
+output=$?
+if [ $output -eq 0 ]; then
+    echo "Saved executable at ./${file_no_ext}"
+else
+    echo "Compilation failed"
+    exit 1
+fi
