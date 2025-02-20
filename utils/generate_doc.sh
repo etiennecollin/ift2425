3baa9b6280b66c4326bc2296bd1cf4c3994161d7
etiennecollin <collin.etienne.contact@gmail.com>
2025-02-19 21:21:57 -0500

    Updated docs generation script

--- utils/generate_doc.sh
+++ utils/generate_doc.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+domain="ift2425.etiennecollin.com"
+docs_path="../docs"
+crate_name="utils"
+
 # Check if arg -o is passed
 if [ "$1" == "-o" ]; then
     cargo doc --workspace --document-private-items --no-deps --open
@@ -7,12 +11,20 @@ else
     cargo doc --workspace --document-private-items --no-deps
 fi
 
-rm -rf ../docs
-mkdir -p ../docs
-cp -r ./target/doc/* ../docs
-rm -rf ../docs/src
-echo "<meta http-equiv=\"refresh\" content=\"0; url=utils/index.html\">" >../docs/index.html
-echo "ift2425.etiennecollin.com" >../docs/CNAME
+# Delete old docs and copy new ones
+rm -rf "$docs_path"
+mkdir -p "$docs_path"
+cp -r ./target/doc/* "$docs_path"
 
-git add ../docs
-git commit -m "Updated docs" ../docs
+# Remove source code from the docs
+# TODO: Remove this line once the semester is over
+rm -rf "$docs_path/src"
+
+# Add a redirect to the index page
+echo "<meta http-equiv=\"refresh\" content=\"0; url=$crate_name/index.html\">" >"$docs_path/index.html"
+
+# Add a CNAME file for GitHub Pages
+echo "$domain" >"$docs_path/CNAME"
+
+git add "$docs_path"
+git commit -m "Updated docs" "$docs_path"
